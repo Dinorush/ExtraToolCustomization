de7366829efe235953f8a0d878638e76aa2eb0bf
Dinorush <onparnorthstar@gmail.com>
2024-07-24 00:34:38 -0400

    Fix shotgun sentry not using fire at target

--- ExtraToolCustomization/Patches/SentryGunPatches_BugFix.cs
+++ ExtraToolCustomization/Patches/SentryGunPatches_BugFix.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace ExtraToolCustomization.Patches
 {
@@ -6,6 +7,7 @@ namespace ExtraToolCustomization.Patches
     internal static class SentryGunPatches_BugFix
     {
         [HarmonyPatch(typeof(SentryGunInstance_Firing_Bullets), nameof(SentryGunInstance_Firing_Bullets.StartFiring))]
+        [HarmonyWrapSafe]
         [HarmonyPostfix]
         private static void Post_StartFiring(SentryGunInstance_Firing_Bullets __instance)
         {
@@ -13,10 +15,36 @@ namespace ExtraToolCustomization.Patches
         }
 
         [HarmonyPatch(typeof(SentryGunInstance_Firing_Bullets), nameof(SentryGunInstance_Firing_Bullets.StopFiring))]
+        [HarmonyWrapSafe]
         [HarmonyPostfix]
         private static void Post_StopFiring(SentryGunInstance_Firing_Bullets __instance)
         {
             __instance.m_burstTimer = Clock.Time + __instance.m_archetypeData.BurstDelay;
+        }
+
+        private static Vector3? _cachedDir = null;
+        [HarmonyPatch(typeof(SentryGunInstance_Firing_Bullets), nameof(SentryGunInstance_Firing_Bullets.TriggerSingleFireAudio))]
+        [HarmonyWrapSafe]
+        [HarmonyPostfix]
+        private static void Pre_ShotgunFireBullet(SentryGunInstance_Firing_Bullets __instance)
+        {
+            if (__instance.m_fireMode != eWeaponFireMode.SentryGunShotgunSemi) return;
+            if (!__instance.m_archetypeData.Sentry_FireTowardsTargetInsteadOfForward || !__instance.m_core.TryGetTargetAimPos(out var pos)) return;
+
+            _cachedDir = __instance.MuzzleAlign.forward;
+            __instance.MuzzleAlign.forward = (pos - __instance.MuzzleAlign.position).normalized;
+        }
+
+        [HarmonyPatch(typeof(SentryGunInstance_Firing_Bullets), nameof(SentryGunInstance_Firing_Bullets.UpdateAmmo))]
+        [HarmonyWrapSafe]
+        [HarmonyPostfix]
+        private static void Post_ShotgunFireBullet(SentryGunInstance_Firing_Bullets __instance)
+        {
+            if (_cachedDir != null)
+            {
+                __instance.MuzzleAlign.forward = (Vector3)_cachedDir;
+                _cachedDir = null;
+            }
         }
     }
 }
