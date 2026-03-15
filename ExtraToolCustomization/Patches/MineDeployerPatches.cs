77abe8de0de50e7f88455635726081221d88f3a0
Dinorush <onparnorthstar@gmail.com>
2026-03-15 03:08:18 -0400

    Add PlacementRange support

--- ExtraToolCustomization/Patches/MineDeployerPatches.cs
+++ ExtraToolCustomization/Patches/MineDeployerPatches.cs
@@ -1,15 +1,20 @@
-﻿using HarmonyLib;
+﻿using AK;
+using ExtraToolCustomization.Networking.MineDeployer;
 using ExtraToolCustomization.ToolData;
 using ExtraToolCustomization.Utils;
-using ExtraToolCustomization.Networking.MineDeployer;
-using SNetwork;
+using HarmonyLib;
 using Player;
+using SNetwork;
+using UnityEngine;
 
 namespace ExtraToolCustomization.Patches
 {
     [HarmonyPatch]
     internal static class MineDeployerPatches
     {
+        private static MineData? _equippedMineData = null;
+        private const float PlacementIndicatorBuffer = 1.5f;
+
         [HarmonyPatch(typeof(MineDeployerFirstPerson), nameof(MineDeployerFirstPerson.OnGearSpawnComplete))]
         [HarmonyWrapSafe]
         [HarmonyPostfix]
@@ -39,6 +44,106 @@ namespace ExtraToolCustomization.Patches
                 deployer.m_timeBetweenPlacements = data.PlacementCooldown;
             }
         }
+
+        [HarmonyPatch(typeof(MineDeployerFirstPerson), nameof(MineDeployerFirstPerson.OnWield))]
+        [HarmonyWrapSafe]
+        [HarmonyPostfix]
+        private static void Post_MineEquipped(MineDeployerFirstPerson __instance)
+        {
+            uint offlineID = __instance.GearIDRange.GetOfflineID();
+            _equippedMineData = ToolDataManager.GetData<MineData>(offlineID, __instance.ItemDataBlock.persistentID, 0);
+        }
+
+        [HarmonyPatch(typeof(MineDeployerFirstPerson), nameof(MineDeployerFirstPerson.Update))]
+        [HarmonyWrapSafe]
+        [HarmonyPrefix]
+        private static bool Pre_Update(MineDeployerFirstPerson __instance)
+        {
+            if (_equippedMineData == null) return true;
+
+            var placementIndicator = __instance.m_placementIndicator;
+            var sound = __instance.Sound;
+            sound.UpdatePosition(__instance.transform.position);
+            if (!__instance.CanWield)
+            {
+                placementIndicator.SetVisible(vis: false);
+                return false;
+            }
+
+            bool canPlace = CheckPlacement(__instance);
+            var owner = __instance.Owner;
+            __instance.m_interactPlaceItem.ManualUpdateWithCondition(canPlace, owner, canPlace);
+            if (canPlace && owner != null)
+            {
+                owner.FPItemHolder.DontRelax();
+                if (__instance.m_pointLight != null)
+                {
+                    __instance.m_pointLight.transform.rotation = Quaternion.LookRotation(__instance.m_lastRayHit.normal * -1f, owner.transform.forward);
+                }
+                if (!__instance.m_lastCanPlace)
+                {
+                    placementIndicator.SetVisible(vis: true);
+                    placementIndicator.SetPlacementEnabled(enabled: true);
+                    __instance.m_lastCanPlace = true;
+                    sound.Post(EVENTS.DEPLOYER_BEEP_TARGET_ACQUIRED);
+                }
+                return false;
+            }
+
+            bool showIndicator = false;
+            if (Clock.Time > __instance.m_indicatorTimer && __instance.m_hasRayHit)
+                showIndicator = __instance.m_lastRayHit.distance < _equippedMineData.PlacementRange + PlacementIndicatorBuffer;
+            if (showIndicator && !__instance.m_lastShowIndicator)
+            {
+                sound.Post(EVENTS.DEPLOYER_BEEP_SURFACE_DETECTED);
+                __instance.m_lastShowIndicator = true;
+            }
+            else if (!showIndicator && __instance.m_lastShowIndicator)
+            {
+                sound.Post(EVENTS.DEPLOYER_BEEP_SURFACE_LOST);
+                __instance.m_lastShowIndicator = false;
+            }
+            placementIndicator.SetVisible(showIndicator);
+            placementIndicator.SetPlacementEnabled(enabled: false);
+            if (__instance.m_lastCanPlace)
+            {
+                __instance.m_lastCanPlace = false;
+                sound.Post(EVENTS.DEPLOYER_BEEP_TARGET_LOST);
+            }
+            return false;
+        }
+
+        private static bool CheckPlacement(MineDeployerFirstPerson __instance)
+        {
+            __instance.m_hasRayHit = false;
+            var owner = __instance.Owner;
+            var camera = owner.FPSCamera;
+            if (!owner.FPItemHolder.ItemHiddenTrigger && !owner.Interaction.HasWorldInteraction && Physics.Raycast(camera.Position, camera.Forward, out var rayHit, _equippedMineData!.PlacementRange + PlacementIndicatorBuffer, LayerManager.MASK_TRIPMINE_CAMERARAY) && !Physics.Linecast(camera.Position, rayHit.point, LayerManager.MASK_TRIPMINE_PLACEMENT_BLOCKERS))
+            {
+                __instance.m_lastRayHit = rayHit;
+                __instance.m_hasRayHit = true;
+                bool validSurface = true;
+                if (__instance.m_requiresGroundSurface)
+                {
+                    validSurface = Vector3.Angle(rayHit.normal, Vector3.up) < __instance.m_maxGroundSurfaceAngleDegrees;
+                }
+                return rayHit.distance < _equippedMineData.PlacementRange && validSurface;
+            }
+
+            return false;
+        }
+
+        [HarmonyPatch(typeof(MineDeployerFirstPerson), nameof(MineDeployerFirstPerson.CheckCanPlace))]
+        [HarmonyWrapSafe]
+        [HarmonyPrefix]
+        private static bool Pre_CheckCanPlace(MineDeployerFirstPerson __instance, ref bool __result)
+        {
+            if (_equippedMineData == null) return true;
+
+            __result = CheckPlacement(__instance);
+            return false;
+        }
+
 
         [HarmonyPatch(typeof(PlayerBotActionDeployTripMine), nameof(PlayerBotActionDeployTripMine.PlaceTripMine))]
         [HarmonyWrapSafe]
--- ExtraToolCustomization/ToolData/MineData.cs
+++ ExtraToolCustomization/ToolData/MineData.cs
@@ -18,6 +18,7 @@ namespace ExtraToolCustomization.ToolData
         public float DamageMin { get; set; } = 0;
         public float DamageMax { get; set; } = 0;
         public float Force { get; set; } = 0;
+        public float PlacementRange { get; set; } = 2.5f;
         public float PlacementTime { get; set; } = 0.5f;
         public float PlacementCooldown { get; set; } = 2f;
         public float PickupTime { get; set; } = 0.5f;
