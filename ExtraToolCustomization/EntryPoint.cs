f0349ff6592c0e349fc95083e6ded16558e0ff9f
Dinorush <onparnorthstar@gmail.com>
2026-03-15 03:08:27 -0400

    Version bump

--- ExtraToolCustomization/EntryPoint.cs
+++ ExtraToolCustomization/EntryPoint.cs
@@ -9,7 +9,7 @@ using GTFO.API;
 
 namespace ExtraToolCustomization
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.6.0")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.6.1")]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency(MTFOWrapper.GUID, BepInDependency.DependencyFlags.SoftDependency)]
     internal sealed class EntryPoint : BasePlugin
